aa30562c897db8050134d7c934f02d436057d97e
bharat-9bit <bharat.yadav@ninebit.in>
2024-04-11 18:07:09 +0530

    add model

--- example.py
+++ example.py
@@ -171,83 +171,143 @@
 # print(result.text)
 
 # importing libraries 
-import speech_recognition as sr 
-import os 
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
-import torch
-from glob import glob
+# import speech_recognition as sr 
+# import os 
+# from pydub import AudioSegment
+# from pydub.silence import split_on_silence
+# import torch
+# from glob import glob
 
-# create a speech recognition object
-r = sr.Recognizer()
+# # create a speech recognition object
+# r = sr.Recognizer()
 
-# a function to recognize speech in the audio file
-# so that we don't repeat ourselves in in other functions
-def transcribe_audio(path):
-    # use the audio file as the audio source
-    with sr.AudioFile(path) as source:
-        audio_listened = r.record(source)
-        # try converting it to text
-        text = r.recognize_google(audio_listened)
-    return text
+# # a function to recognize speech in the audio file
+# # so that we don't repeat ourselves in in other functions
+# def transcribe_audio(path):
+#     # use the audio file as the audio source
+#     with sr.AudioFile(path) as source:
+#         audio_listened = r.record(source)
+#         # try converting it to text
+#         text = r.recognize_google(audio_listened)
+#     return text
 
-# a function that splits the audio file into chunks on silence
-# and applies speech recognition
-def get_large_audio_transcription_on_silence(path):
-    """Splitting the large audio file into chunks
-    and apply speech recognition on each of these chunks"""
-    # open the audio file using pydub
-    sound = AudioSegment.from_file(path)  
-    # split audio sound where silence is 500 miliseconds or more and get chunks
-    chunks = split_on_silence(sound,
-        # experiment with this value for your target audio file
-        min_silence_len = 500,
-        # adjust this per requirement
-        silence_thresh = sound.dBFS-14,
-        # keep the silence for 1 second, adjustable as well
-        keep_silence=500,
-    )
-    folder_name = "audio-chunks"
-    # create a directory to store the audio chunks
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
-    whole_text = ""
-    # process each chunk 
-    for i, audio_chunk in enumerate(chunks, start=1):
-        # export audio chunk and save it in
-        # the `folder_name` directory.
-        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
-        audio_chunk.export(chunk_filename, format="wav")
-        # recognize the chunk
-        try:
-            text = transcribe_audio(chunk_filename)
-        except sr.UnknownValueError as e:
-            print("Error:", str(e))
-        else:
-            text = f"{text.capitalize()}. "
-            # print(chunk_filename, ":", text)
-            whole_text += text
-    # return the text for all chunks detected
-    return whole_text
+# # a function that splits the audio file into chunks on silence
+# # and applies speech recognition
+# def get_large_audio_transcription_on_silence(path):
+#     """Splitting the large audio file into chunks
+#     and apply speech recognition on each of these chunks"""
+#     # open the audio file using pydub
+#     sound = AudioSegment.from_file(path)  
+#     # split audio sound where silence is 500 miliseconds or more and get chunks
+#     chunks = split_on_silence(sound,
+#         # experiment with this value for your target audio file
+#         min_silence_len = 500,
+#         # adjust this per requirement
+#         silence_thresh = sound.dBFS-14,
+#         # keep the silence for 1 second, adjustable as well
+#         keep_silence=500,
+#     )
+#     folder_name = "audio-chunks"
+#     # create a directory to store the audio chunks
+#     if not os.path.isdir(folder_name):
+#         os.mkdir(folder_name)
+#     whole_text = ""
+#     # process each chunk 
+#     for i, audio_chunk in enumerate(chunks, start=1):
+#         # export audio chunk and save it in
+#         # the `folder_name` directory.
+#         chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+#         audio_chunk.export(chunk_filename, format="wav")
+#         # recognize the chunk
+#         try:
+#             text = transcribe_audio(chunk_filename)
+#         except sr.UnknownValueError as e:
+#             print("Error:", str(e))
+#         else:
+#             text = f"{text.capitalize()}. "
+#             # print(chunk_filename, ":", text)
+#             whole_text += text
+#     # return the text for all chunks detected
+#     return whole_text
 
-path = "voice3.wav"
-print("\nFull text:", get_large_audio_transcription_on_silence(path))
+# path = "voice3.wav"
+# print("\nFull text:", get_large_audio_transcription_on_silence(path))
 
 
-# Perform speech-to-text conversion using the pytorch library
-device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
-model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                       model='silero_stt',
-                                       language='en', # also available 'de', 'es'
-                                       device=device)
-(read_batch, split_into_batches,
- read_audio, prepare_model_input) = utils  # see function signature for details
+# # Perform speech-to-text conversion using the pytorch library
+# device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
+# model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
+#                                        model='silero_stt',
+#                                        language='en', # also available 'de', 'es'
+#                                        device=device)
+# (read_batch, split_into_batches,
+#  read_audio, prepare_model_input) = utils  # see function signature for details
 
-test_files = glob('voice3.wav')
-batches = split_into_batches(test_files, batch_size=10)
-input = prepare_model_input(read_batch(batches[0]),
-                            device=device)
+# test_files = glob('voice3.wav')
+# batches = split_into_batches(test_files, batch_size=10)
+# input = prepare_model_input(read_batch(batches[0]),
+#                             device=device)
 
-output = model(input)
-for example in output:
-    print(decoder(example.cpu()))
+# output = model(input)
+# for example in output:
+#     print(decoder(example.cpu()))
+
+# from faster_whisper import WhisperModel
+
+# model_size = "large-v3"
+
+# # Run on GPU with FP16
+# model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+# # or run on GPU with INT8
+# # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+# # or run on CPU with INT8
+# # model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+# segments, info = model.transcribe("voice1.mp3", beam_size=5)
+
+# print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+# for segment in segments:
+#     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+####################Model - whisper##########
+
+import whisper
+
+# model = whisper.load_model("base")
+# result = model.transcribe("samyak1.mp4")
+# print(result["text"])
+
+####################Model - faster whisper##########
+
+# from faster_whisper import WhisperModel
+
+# model = WhisperModel("distil-medium.en")
+
+# segments, info = model.transcribe("samyak1.mp4")
+# for segment in segments:
+#     # print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+#     print(segment.text)
+
+
+# ############################## speech to text - torch#######################################################
+# import torch
+# from glob import glob
+
+# device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
+# model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
+#                                        model='silero_stt',
+#                                        language='en', # also available 'de', 'es'
+#                                        device=device)
+# (read_batch, split_into_batches,
+#  read_audio, prepare_model_input) = utils 
+
+# test_files = glob('samyak1.mp4')
+# batches = split_into_batches(test_files, batch_size=10)
+# input = prepare_model_input(read_batch(batches[0]),
+#                             device=device)
+
+# output = model(input)
+# for example in output:
+#     print(decoder(example.cpu()))
